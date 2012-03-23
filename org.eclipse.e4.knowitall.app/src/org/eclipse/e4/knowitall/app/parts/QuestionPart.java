207c764a96c9d899d6b4ce09ec8f3a504bf269de
scela <sopotcela@gmail.com>
2012-03-24 00:52:50 +0100

    Added conditional mocking support

--- org.eclipse.e4.knowitall.app/src/org/eclipse/e4/knowitall/app/parts/QuestionPart.java
+++ org.eclipse.e4.knowitall.app/src/org/eclipse/e4/knowitall/app/parts/QuestionPart.java
@@ -13,13 +13,16 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import static org.mockito.Mockito.*;
+
 public class QuestionPart extends Composite implements SelectionListener {
+	private static final boolean mocking = true;
 	private Text questionText;
 	private Text answerText;
 	private final IKnowItAll knowItAll;
 
 	/**
 	 * Create the composite.
+	 * 
 	 * @param parent
 	 * @param style
 	 */
@@ -27,15 +30,15 @@ public class QuestionPart extends Composite implements SelectionListener {
 	public QuestionPart(Composite parent, @Optional IKnowItAll knowItAll) {
 		super(parent, SWT.NONE);
 		this.knowItAll = knowItAll;
-		
+
 		questionText = new Text(this, SWT.BORDER);
 		questionText.setBounds(10, 38, 266, 21);
-		
+
 		Button btnFindOut = new Button(this, SWT.NONE);
 		btnFindOut.addSelectionListener(this);
 		btnFindOut.setBounds(282, 38, 75, 25);
 		btnFindOut.setText("Find out!");
-		
+
 		answerText = new Text(this, SWT.BORDER | SWT.MULTI);
 		answerText.setEditable(false);
 		answerText.setBounds(10, 64, 348, 175);
@@ -50,15 +53,21 @@ public class QuestionPart extends Composite implements SelectionListener {
 	@Override
 	public void widgetSelected(SelectionEvent e) {
 		widgetDefaultSelected(e);
-		
+
 	}
 
 	@Override
 	public void widgetDefaultSelected(SelectionEvent e) {
-		IKnowItAll knowItAll = mock(IKnowItAll.class);
-		when(knowItAll.answer("What?")).thenReturn("Ca bone");
-		when(knowItAll.answer("When?")).thenReturn("Ja ime");
-		when(knowItAll.answer("How?")).thenReturn("Shpejt e shpejt");
-		answerText.append(knowItAll.answer(questionText.getText())+"\n");
+		if (mocking) {
+			IKnowItAll knowItAll = mock(IKnowItAll.class);
+			when(knowItAll.answer("What?")).thenReturn("Ca bone");
+			when(knowItAll.answer("When?")).thenReturn("Ja ime");
+			when(knowItAll.answer("How?")).thenReturn("Shpejt e shpejt");
+
+			answerText.append(knowItAll.answer(questionText.getText()) + "\n");
+		} else {
+			answerText.append(knowItAll.answer(questionText.getText()) + "\n");
+
+		}
 	}
 }
