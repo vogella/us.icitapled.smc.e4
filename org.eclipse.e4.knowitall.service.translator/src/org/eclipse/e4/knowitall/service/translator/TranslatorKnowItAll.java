e6d944292cc31645345a30fd7e6ef34c9c8b24f0
scela <sopotcela@gmail.com>
2012-03-24 21:01:34 +0100

    Added translation implementation.

---
+++ org.eclipse.e4.knowitall.service.translator/src/org/eclipse/e4/knowitall/service/translator/TranslatorKnowItAll.java
@@ -0,0 +1,187 @@
+package org.eclipse.e4.knowitall.service.translator;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import com.memetix.mst.language.Language;
+import com.memetix.mst.language.SpokenDialect;
+import com.memetix.mst.speak.Speak;
+import com.memetix.mst.translate.Translate;
+
+import knowitallservice.IKnowItAll;
+
+public class TranslatorKnowItAll implements IKnowItAll {
+	
+	private String question;
+	private String answer;
+
+	public TranslatorKnowItAll() {
+		try {
+			Speak.setKey("29AEA70CEE1293E98960A42A5B3AEF135987624C");
+			Speak.execute("Testing", SpokenDialect.CATALAN_SPAIN);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public EClass eClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource eResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EObject eContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EStructuralFeature eContainingFeature() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EReference eContainmentFeature() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EList<EObject> eContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeIterator<EObject> eAllContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eIsProxy() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public EList<EObject> eCrossReferences() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object eGet(EStructuralFeature feature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object eGet(EStructuralFeature feature, boolean resolve) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eSet(EStructuralFeature feature, Object newValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean eIsSet(EStructuralFeature feature) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void eUnset(EStructuralFeature feature) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object eInvoke(EOperation operation, EList<?> arguments)
+			throws InvocationTargetException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EList<Adapter> eAdapters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eDeliver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void eSetDeliver(boolean deliver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	@Override
+	public void eNotify(Notification notification) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public String answer(String question) {
+		Translate.setKey("29AEA70CEE1293E98960A42A5B3AEF135987624C");
+		try {
+			
+			
+			String language = question.substring(question.lastIndexOf(" ")+1);
+			String word = question.substring(question.indexOf(" ")+1, question.lastIndexOf(" in"));
+//			System.err.println("Word: "+word);
+//			System.err.println("Language: "+language);
+			return language +" word for "+word+" is "+Translate.execute(word.toLowerCase(), Language.valueOf(language.toUpperCase()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Problem occured while translating";
+		}
+	}
+
+}
